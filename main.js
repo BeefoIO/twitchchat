0098b5698f93d2b41d986d401d9d00eb332afff8
Benjamin Grziwok <gerbeefo@gmail.com>
2018-11-01 00:00:43 +0100

    autostart service.js with electron

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const electron = require('electron');
 const url = require('url');
 const path = require('path');
 const net = require('net');
+require('./service');
 
 const {app, BrowserWindow, Menu, ipcMain} = electron;
 
