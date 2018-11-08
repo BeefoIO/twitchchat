d36b0777d99aed169cd9643fce5df53d6970992c
Benjamin Grziwok <gerbeefo@gmail.com>
2018-11-08 23:55:43 +0100

    implemented settings.json only commit this time

--- json.js
+++
@@ -1,7 +0,0 @@
-var fs = require('fs');
-
-var settings = fs.readFileSync('./settings.json', 'utf8');
-console.log(settings);
-var settingsContent = JSON.parse(settings);
-
-console.log(settingsContent);
